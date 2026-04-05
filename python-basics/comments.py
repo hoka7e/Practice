f6c0aaf5186bb6d4b1dbf076476404e24e9b569a
Ilya <assa.tresh08@gmail.com>
2026-04-05 12:00:00 +0500

    Practice9

--- python-basics/comments.py
+++
@@ -1,5 +0,0 @@
-# Comments starts with a #, and Python will ignore them:
-# Comments can be used to explain Python code.
-# Comments can be used to make the code more readable.
-
-print("Hello, World!") #This is a comment
